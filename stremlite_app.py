2d36a5527ff3b8b3a3984b382fb7a85e6d569aad
Amriksinh <amriksinh@gmail.com>
2022-08-14 19:09:04 +0530

    Update stremlite_app.py

--- stremlite_app.py
+++ stremlite_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents Having a healthy Dinner')
 streamlit.header('Breakfast meanu')
 streamlit.text('Tea with bread')
-streamlit.text('Egg with Bread')
+Streamlit.text('Egg with Bread')
 streamlit.text('new food')
